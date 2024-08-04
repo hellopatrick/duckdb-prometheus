f122e4f7b563c3ec00a50844292fe6c9f5200e9e
hellopatrick; Patrick Rogers <hellopatrick@users.noreply.github.com>
2024-08-04 09:30:50 -0700

    ok

--- Makefile
+++ Makefile
@@ -6,3 +6,8 @@ EXT_CONFIG=${PROJ_DIR}extension_config.cmake
 
 # Include the Makefile from extension-ci-tools
 include extension-ci-tools/makefiles/duckdb_extension.Makefile
+
+.PHONY: lint
+lint:
+	clang-format -i src/*.cpp
+	clang-format -i src/include/*.hpp
--- src/prometheus_extension.cpp
+++ src/prometheus_extension.cpp
@@ -1,47 +1,88 @@
+#include "duckdb/function/table_function.hpp"
 #define DUCKDB_EXTENSION_MAIN
 
-#include "prometheus_extension.hpp"
 #include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
+#include "duckdb/common/helper.hpp"
 #include "duckdb/common/string_util.hpp"
+#include "duckdb/common/types.hpp"
+#include "duckdb/common/types/value.hpp"
 #include "duckdb/function/scalar_function.hpp"
 #include "duckdb/main/extension_util.hpp"
-#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+#include "prometheus_extension.hpp"
 
-// OpenSSL linked through vcpkg
-#include <openssl/opensslv.h>
+#include <cstdio>
+#include <iostream>
 
 namespace duckdb {
 
-inline void PrometheusScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Prometheus "+name.GetString()+" 🐥");;
-        });
+struct PrometheusFunctionBindData : public TableFunctionData {
+	std::string query;
+
+	PrometheusFunctionBindData(std::string q) {
+		query = q;
+	};
+};
+
+unique_ptr<FunctionData> PrometheusBindFunction(ClientContext &context, TableFunctionBindInput &input,
+                                                vector<LogicalType> &return_types, vector<string> &names) {
+	auto qry = StringValue::Get(input.inputs[0]);
+
+	std::string api_url = "http://localhost:9090/api/v1/query";
+	auto api_url_entry = input.named_parameters.find("api_url");
+	if (api_url_entry != input.named_parameters.end()) {
+		api_url = StringValue::Get(api_url_entry->second);
+	}
+
+	return_types.emplace_back(LogicalType::TIMESTAMP);
+	names.emplace_back("timestamp");
+
+	return_types.emplace_back(LogicalType::VARCHAR);
+	names.emplace_back("__name__");
+
+	return_types.emplace_back(LogicalType::MAP(LogicalType::VARCHAR, LogicalType::VARCHAR));
+	names.emplace_back("labels");
+
+	return_types.emplace_back(LogicalType::FLOAT);
+	names.emplace_back("value");
+
+	auto res = make_uniq<PrometheusFunctionBindData>(qry);
+
+	return std::move(res);
 }
 
-inline void PrometheusOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-    auto &name_vector = args.data[0];
-    UnaryExecutor::Execute<string_t, string_t>(
-	    name_vector, result, args.size(),
-	    [&](string_t name) {
-			return StringVector::AddString(result, "Prometheus " + name.GetString() +
-                                                     ", my linked OpenSSL version is " +
-                                                     OPENSSL_VERSION_TEXT );;
-        });
+struct GlobalPrometheusTableFunctionState : public GlobalTableFunctionState {};
+
+unique_ptr<GlobalTableFunctionState> PrometheusGlobalStateFunction(ClientContext &context,
+                                                                   TableFunctionInitInput &input) {
+	auto res = make_uniq<GlobalPrometheusTableFunctionState>();
+
+	return std::move(res);
+}
+
+struct LocalPrometheusTableFunctionState : public LocalTableFunctionState {};
+
+unique_ptr<LocalTableFunctionState> PrometheusLocalStateFunction(ExecutionContext &context,
+                                                                 TableFunctionInitInput &input,
+                                                                 GlobalTableFunctionState *global_state) {
+	auto res = make_uniq<LocalPrometheusTableFunctionState>();
+
+	return std::move(res);
+}
+
+void PrometheusTableFunction(ClientContext &context, TableFunctionInput &data, DataChunk &output) {
 }
 
 static void LoadInternal(DatabaseInstance &instance) {
-    // Register a scalar function
-    auto prometheus_scalar_function = ScalarFunction("prometheus", {LogicalType::VARCHAR}, LogicalType::VARCHAR, PrometheusScalarFun);
-    ExtensionUtil::RegisterFunction(instance, prometheus_scalar_function);
+	auto &config = DBConfig::GetConfig(instance);
 
-    // Register another scalar function
-    auto prometheus_openssl_version_scalar_function = ScalarFunction("prometheus_openssl_version", {LogicalType::VARCHAR},
-                                                LogicalType::VARCHAR, PrometheusOpenSSLVersionScalarFun);
-    ExtensionUtil::RegisterFunction(instance, prometheus_openssl_version_scalar_function);
+	auto prometheus_table_function =
+	    TableFunction("prometheus", {LogicalType::VARCHAR}, PrometheusTableFunction, PrometheusBindFunction,
+	                  PrometheusGlobalStateFunction, PrometheusLocalStateFunction);
+
+	prometheus_table_function.named_parameters["api_url"] = LogicalType::VARCHAR;
+
+	ExtensionUtil::RegisterFunction(instance, prometheus_table_function);
 }
 
 void PrometheusExtension::Load(DuckDB &db) {
@@ -52,6 +93,7 @@ std::string PrometheusExtension::Name() {
 }
 
 std::string PrometheusExtension::Version() const {
+
 #ifdef EXT_VERSION_PROMETHEUS
 	return EXT_VERSION_PROMETHEUS;
 #else
@@ -64,8 +106,8 @@ std::string PrometheusExtension::Version() const {
 extern "C" {
 
 DUCKDB_EXTENSION_API void prometheus_init(duckdb::DatabaseInstance &db) {
-    duckdb::DuckDB db_wrapper(db);
-    db_wrapper.LoadExtension<duckdb::PrometheusExtension>();
+	duckdb::DuckDB db_wrapper(db);
+	db_wrapper.LoadExtension<duckdb::PrometheusExtension>();
 }
 
 DUCKDB_EXTENSION_API const char *prometheus_version() {
